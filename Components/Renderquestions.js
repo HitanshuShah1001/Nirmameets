213d40ce263354d1309ce859287a29d88b637ebb
Hitanshu Shah <hitanshushah@Hitanshus-MacBook-Air.local>
2022-09-04 01:45:16 +0530

    feat:Prevented users from upvoting,downvoting their own answer

--- Components/Renderquestions.js
+++ Components/Renderquestions.js
@@ -1,88 +1,105 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { View, Text, Pressable, Button, Modal, TextInput, Alert, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  Button,
+  Modal,
+  TextInput,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { AppContext } from "../Context/Context";
 import { logoutUser } from "../Redux/Action/Actions";
 import { useNavigation } from "@react-navigation/native";
 
-const RenderQuestions = ({ question, answers,toRefresh }) => {
+const RenderQuestions = ({ question, answers, toRefresh }) => {
   const navigation = useNavigation();
-  const { setRefreshquestions,refreshQuestions } = React.useContext(AppContext);
+  const { setRefreshquestions, refreshQuestions } =
+    React.useContext(AppContext);
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user)
-  const [answer,setAnswer] = useState("Enter answer here");
+  const user = useSelector((state) => state.user);
+  const [answer, setAnswer] = useState("Enter answer here");
   const [showanswers, setShowanswers] = useState(false);
   const [addcommentmodalvisible, setAddcommentmodalvisible] = useState(false);
-  const [tnheight,setTnheight] = useState(0);
+  const [tnheight, setTnheight] = useState(0);
 
   const postanswer = () => {
     toRefresh(false);
-    axios.post(`http://localhost:443/addanswer/${question._id}`,{
-      token:user.token,
-      answer:answer,
-      Username:user.Username
-    }).then(res => {
-      toRefresh(Date.now());
-      Alert.alert(res.data.message);
-      
-    }).catch(error => {
-      if(error?.response?.status===401){
-          dispatch(() => logoutUser())
+    axios
+      .post(`http://localhost:443/addanswer/${question._id}`, {
+        token: user.token,
+        answer: answer,
+        Username: user.Username,
+      })
+      .then((res) => {
+        toRefresh(Date.now());
+        Alert.alert(res.data.message);
+      })
+      .catch((error) => {
+        if (error?.response?.status === 401) {
+          dispatch(() => logoutUser());
+        } else {
+          console.log(error.response);
+          Alert.alert(error.message);
         }
-      else{
-        console.log(error.response);
-        Alert.alert(error.message);
-      }
-  })
-  }
+      });
+  };
 
-  const addupvote = (id) => {
-    
-    axios.post(`http://localhost:443/addupvote/${question._id}`,{
-      token:user.token,
-      id:id
-    }).then(res => {
-      toRefresh(Date.now());
-    }).catch(error => {
-      console.log(error);
-      if(error?.response?.status === 401){
-        dispatch(() => logoutUser())
-      }
-      else{
-        console.log(error.response);
-      }
-    })
-  }
+  const addupvote = (id, Username) => {
+    console.log(user.Username, question.Username);
+    if (user.Username === Username) {
+      Alert.alert("You cannot upvote your own answer");
+    } else {
+      axios
+        .post(`http://localhost:443/addupvote/${question._id}`, {
+          token: user.token,
+          id: id,
+        })
+        .then((res) => {
+          toRefresh(Date.now());
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error?.response?.status === 401) {
+            dispatch(() => logoutUser());
+          } else {
+            console.log(error.response);
+          }
+        });
+    }
+  };
 
-  const adddownvote = (id) => {
-    toRefresh(false);
-    axios.post(`http://localhost:443/adddownvote/${question._id}`,{
-      token:user.token,
-      id:id
-    }).then(res => {
-      toRefresh(Date.now());
-    }).catch(error => {
-      if(error?.response?.status === 401){
-        dispatch(() => logoutUser());
-      }
-      else{
-        console.log(error.response);
-      }
-    })
-  }
-
-  
+  const adddownvote = (id, Username) => {
+    if (user.Username === Username) {
+      Alert.alert("You cannot downvote your own answer");
+    } else {
+      axios
+        .post(`http://localhost:443/adddownvote/${question._id}`, {
+          token: user.token,
+          id: id,
+        })
+        .then((res) => {
+          toRefresh(Date.now());
+        })
+        .catch((error) => {
+          if (error?.response?.status === 401) {
+            dispatch(() => logoutUser());
+          } else {
+            console.log(error.response);
+          }
+        });
+    }
+  };
 
   const getAnswers = () => {
     setShowanswers(!showanswers);
   };
 
   return (
-    <View
-      key={question._id}
-      style={{ marginTop: 30, alignItems: "center" }}
-    >
+    <View key={question._id} style={{ marginTop: 30, alignItems: "center" }}>
       <Modal visible={addcommentmodalvisible} animationType="slide">
         <View style={{ flex: 1, marginTop: 30 }}>
           {answers?.map((answer, index) => (
@@ -99,32 +116,34 @@ const RenderQuestions = ({ question, answers,toRefresh }) => {
               <Text>{Object.values(answer)[0]}</Text>
             </View>
           ))}
-          <View style={{position:'absolute',bottom:40,width:'100%'}}>
+          <View style={{ position: "absolute", bottom: 40, width: "100%" }}>
             <TextInput
               style={{
                 position: "absolute",
                 bottom: 40,
-                height: Math.max(50,tnheight),
+                height: Math.max(50, tnheight),
                 borderRadius: 10,
-                width:'90%',
-                alignSelf:'center',
+                width: "90%",
+                alignSelf: "center",
                 backgroundColor: "grey",
-                paddingHorizontal:20,
-                paddingVertical:10
-              
+                paddingHorizontal: 20,
+                paddingVertical: 10,
               }}
               multiline={true}
               onContentSizeChange={(event) => {
-                setTnheight(event.nativeEvent.contentSize.height)
+                setTnheight(event.nativeEvent.contentSize.height);
                 console.log(tnheight);
               }}
               placeholder="Enter answer here"
               value={answer}
               onChangeText={(text) => setAnswer(text)}
             />
-            <Button title="Post" style={{position:'absolute',bottom:10}} onPress={() => postanswer()}/>
-            </View>
-         
+            <Button
+              title="Post"
+              style={{ position: "absolute", bottom: 10 }}
+              onPress={() => postanswer()}
+            />
+          </View>
         </View>
       </Modal>
       <Text>{question.question}</Text>
@@ -137,37 +156,38 @@ const RenderQuestions = ({ question, answers,toRefresh }) => {
         />
       </View>
       {showanswers && (
-        <ScrollView >
-          {
-            answers?.map((answer, index) => (
-          
-              <View
-                key={index}
-                style={{
-                  marginTop: 30,
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                  paddingHorizontal: 20,
+        <ScrollView>
+          {answers?.map((answer, index) => (
+            <View
+              key={index}
+              style={{
+                marginTop: 30,
+                justifyContent: "center",
+                alignItems: "flex-start",
+                paddingHorizontal: 20,
+              }}
+            >
+              <Text>{Object.getOwnPropertyNames(answer)[0]}</Text>
+              <Text>{Object.values(answer)[0]}</Text>
+              <Pressable
+                onPress={() => {
+                  addupvote(answer.id, Object.values(answer)[0]);
                 }}
               >
-                <Text>{Object.getOwnPropertyNames(answer)[0]}</Text>
-                <Text>{Object.values(answer)[0]}</Text>
-                <Pressable onPress={() => {addupvote(answer.id)}}>
-                  <Text>Upvote</Text>
-                  </Pressable>
-                  <Text>{answer?.Votes}</Text>
-                  <Pressable onPress={() => {adddownvote(answer.id)}}>
-                  <Text>Downvote</Text>
-                  </Pressable>
-
-                  
-
-              </View>
-            ))}
-          
-          </ScrollView>
+                <Text>Upvote</Text>
+              </Pressable>
+              <Text>{answer?.Votes}</Text>
+              <Pressable
+                onPress={() => {
+                  adddownvote(answer.id, Object.values(answer)[0]);
+                }}
+              >
+                <Text>Downvote</Text>
+              </Pressable>
+            </View>
+          ))}
+        </ScrollView>
       )}
-        
     </View>
   );
 };
